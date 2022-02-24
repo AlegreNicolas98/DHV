f4db174677e8c2cdae250781964a6f94a0b30805
Alegre Nicolas <nicolasalegre4@gmail.com>
2022-02-24 05:31:45 -0300

    3 breakpoints terminados

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 
 toggle.addEventListener("click", () => {
- menu.classList.toggle(".visible");
+ menu.classList.toggle("visible");
 
 if(menu.classList.contains("visible")){
  toggle.setAttribute("aria-label", "cerrar menu");
